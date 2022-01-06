ac5317f11589167489c043e8cd1dbddb03d0a47e
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2022-01-06 14:37:49 +0530

    [Santosh]-Added:Files for Creating signin-page

---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+export class Signup extends Component{
+    render(){
+        return(
+            <div>
+                <h2>LoginPage</h2>
+            </div>
+        )
+    }
+}
+
+export default Signup
