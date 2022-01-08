9d3ffb8cb2a631306ead29c3434737918fe16f48
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2022-01-08 15:42:13 +0530

    [Santosh]Added:Code for Signup-Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-// import Signup from './pages/signup/Signup';
+import Signup from './pages/signup/Signup';
 import Signin from './pages/signin/Signin';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Signup /> */}
+      <Signup />
       <Signin />
     </div>
   );
