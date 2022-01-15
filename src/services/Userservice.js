f4eaab949cb7d5770b92e549a723c7b107f09842
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2022-01-15 13:43:55 +0530

    [Santosh]Added: Axios code for User Service and Axios Service

---
+++ src/services/Userservice.js
@@ -0,0 +1,20 @@
+import Signup from "../pages/signup/Signup";
+import AxiosService from "./axiosService";
+
+const service= new AxiosService();
+
+let baseUrl = 'http://fundoonotes.incubation.bridgelabz.com/api/';
+
+
+class UserService{
+    registration(data){
+       return service.postMethod(`${baseUrl}user/userSignUp`,data);
+    }
+
+    signin(data){
+        return service.postMethod(`http://fundoonotes.incubation.bridgelabz.com/api/user/login`,data);
+     }
+}
+
+
+export default UserService
