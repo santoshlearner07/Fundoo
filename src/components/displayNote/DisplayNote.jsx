c0f83aea531c048b1b7fb7177f3527b3326acbfe
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2022-01-22 00:53:27 +0530

    [Santosh]Added: Hovering of Display Box(onclicking it gets big)

--- src/components/displayNote/DisplayNote.jsx
+++ src/components/displayNote/DisplayNote.jsx
@@ -1,32 +1,91 @@
-import React, { Component } from "react";
-import { Button } from "@material-ui/core";
-
-import '../displayNote/DisplayNote.scss'
+import React, { Component } from 'react';
+import './DisplayNote.scss';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import Button from '@mui/material/Button';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import Icon from "../icons/Icon";
-import NoteService from "../../services/noteService";
 
-const noteService = new NoteService();
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+        width: theme.spacing(80)
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+        width: theme.spacing(100)
+    },
+}));
+
+const BootstrapDialogTitle = (props) => {
+    const { children, onClose, ...other } = props;
+
+    return (
+        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+            {children}
+            {onClose ? (
+                <IconButton
+                    aria-label="close"
+                    onClick={onClose}
+                    sx={{
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: (theme) => theme.palette.grey[500],
+                    }}
+                >
+                    <CloseIcon />
+                </IconButton>
+            ) : null}
+        </DialogTitle>
+    );
+};
+
+BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+};
+
 export class DisplayNote extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
+            open: false,
+            title: this.props.noteArray.title,
+            description: this.props.noteArray.description
+        }
+    }
 
-        };
+    handleOpenTitle = (item) => {
+        this.setState({
+            open: true,
+            title: item.title,
+            description: item.description
+        })
+        console.log(this.state.title)
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false,
+        })
     }
 
     render() {
         console.log("this is Display Note")
         return (
-
-            <div className="mainDisplay">
-
+            <div className='mainDisplay'>
                 {this.props.noteArray.map((item, index) => (
-                    < div className="displayBox">
+                    <div className='displayBox' onClick={() => this.handleOpenTitle(item)}>
+                        <div className='titleDescrip'>
 
-                        <div className="titleDescrip">
-                            {item.title}  <br></br>
+                            {item.title}<br></br>
                             {item.description}
+
                         </div>
                         <div className="displayThirdPart">
                             <div className="displayIcons">
@@ -35,6 +94,26 @@ export class DisplayNote extends Component {
                         </div>
                     </div>
                 ))}
+                <BootstrapDialog onClose={this.handleClose} aria-labelledby="customized-dialog-title" open={this.state.open}>
+                    <div style={{ width: "100%", overflow: "hidden" }}>
+                        <BootstrapDialogTitle id="customized-dialog-title" onClose={this.handleClose} >
+
+                            Title-1
+
+                        </BootstrapDialogTitle>
+                        <DialogContent>
+
+                            Description
+
+                        </DialogContent>
+                        <DialogContent className="close-Icon" >
+
+                            <Icon />
+                            <Button autoFocus onClick={(title, description) => this.handleClose(title, description)}> Close </Button>
+                        </DialogContent>
+
+                    </div>
+                </BootstrapDialog>
             </div>
         )
     }
